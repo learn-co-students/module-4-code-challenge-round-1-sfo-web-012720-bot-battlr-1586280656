eae99c4b38202b38bf1dcaf274f587a20e8851d3
austinoso <osorio_austin@yahoo.com>
2020-04-07 11:25:34 -0700

    finished code challenge

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,18 +1,25 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import BotCard from '../components/BotCard';
 
 class BotCollection extends Component {
-  //your code here
+	//your code here
 
-  render() {
-    return (
-      <div className="ui four column grid">
-        <div className="row">
-          {/*...and here..*/}
-          Collection of all bots
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="ui four column grid">
+				<div className="row">
+					{this.props.bots.map((bot) => (
+						<BotCard
+							handleRemove={this.props.handleRemove}
+							handleClick={this.props.addToArmy}
+							key={bot.id}
+							bot={bot}
+						/>
+					))}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default BotCollection;
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -1,20 +1,27 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import BotCard from '../components/BotCard';
 
 class YourBotArmy extends Component {
-  //your bot army code here...
+	//your bot army code here...
 
-  render() {
-    return (
-      <div className="ui segment inverted olive bot-army">
-        <div className="ui five column grid">
-          <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="ui segment inverted olive bot-army">
+				<div className="ui five column grid">
+					<div className="row bot-army-row">
+						{this.props.bots.map((bot) => (
+							<BotCard
+								handleRemove={this.props.handleRemove}
+								handleClick={this.props.removeFromArmy}
+								key={bot.id}
+								bot={bot}
+							/>
+						))}
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default YourBotArmy;
